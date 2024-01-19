5fac30f800add5b36b5c73d92233d7c914ab2c87
Nano <alhernano@gmail.com>
2024-01-19 09:20:23 -0500

    added a new validation

--- js/comments.js
+++ js/comments.js
@@ -8,6 +8,9 @@ form.addEventListener('submit', (e) =>{
     if(comentario.value.length >200){
         messages.push('El comentario no puede superar los 200 caracteres.')
     }
+    if(comentario.value.length =0){
+        messages.push('Se necesita ingresar un comentario.')
+    }
     if (messages.length > 0){
         e.preventDefault()
         errorElement.innerText = messages.join(', ')
